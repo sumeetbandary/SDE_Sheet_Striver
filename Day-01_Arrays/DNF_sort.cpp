56f8ad62228b7d718bcf6498a54c283cd89bb54f
Sumeet <59203367+sumeetbandary@users.noreply.github.com>
2021-08-06 19:05:09 +0530

    Update DNF_sort.cpp

--- Day-01_Arrays/DNF_sort.cpp
+++ Day-01_Arrays/DNF_sort.cpp
@@ -1,14 +1,15 @@
 /*
-    problem link: https://leetcode.com/problems/sort-colors/
+    problem link: 
+    https://leetcode.com/problems/sort-colors/
 */ 
 
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        // use DNF sort
-        // region of 0s{0, low}, region of 1s{low, mid}, region of 2s{high, n - 1}
-        // uncertain region or unsorted region{mid, high}.
-        // we need to shrink this region{mid, high}.
+        //use DNF sort
+        //region of 0s{0, low}, region of 1s{low, mid}, region of 2s{high, n - 1}
+        //uncertain region or unsorted region{mid, high}.
+        //we need to shrink this region{mid, high}.
         int n = nums.size();
         int low = 0, mid = 0, high = n - 1;
         while(mid <= high) {
