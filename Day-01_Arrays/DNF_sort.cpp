94e3835071945998a62396e56e5569bed0ac624c
Sumeet <59203367+sumeetbandary@users.noreply.github.com>
2021-08-07 09:28:33 +0530

    Update DNF_sort.cpp

--- Day-01_Arrays/DNF_sort.cpp
+++ Day-01_Arrays/DNF_sort.cpp
@@ -7,25 +7,61 @@
     https://www.youtube.com/watch?v=oaVa-9wmpns&list=PLgUwDviBIf0rPG3Ictpu74YWBQ1CaBkm2&index=3
 */ 
 
- /* Using the sorting function 
+ /* 
+    Using the sorting function 
     TC - O(nlogn)
     SC - O(n)
 */
 void sort012(int a[], int n)
 {
+    // Using STL sort function
     sort(a,a+n);
 }
 
 /*
-    Using Counting Mthod
+    Using Counting Method
+    Count no of 0s, 1s and 2s and then insert in the array
+    TC - O(2n)
+    SC - O(1)
 */
+void sort012(int a[], int n)
+{
+    int count0=0,count1=0,count2=0;
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]==0)
+            count0++;
+        else if(a[i]==1)
+            count1++;
+        else
+            count2++;
+    }
+    int x=0;
+    for(int i=1;i<=count0;i++)
+    {
+        a[x]=0;
+        x++;
+    }
+    for(int i=1;i<=count1;i++)
+    {
+        a[x]=1;
+        x++;
+    }
+    for(int i=1;i<=count2;i++)
+    {
+        a[x]=2;
+        x++;
+    } 
+}
 
 /*
     Using Dutch National Flag 
+    Three pointers low, mid and high
+    TC - O(n)
+    SC - O(1)
 */
 void sortColors(vector<int>& nums) 
     {
-        //use DNF sort
         //region of 0s{0, low}, region of 1s{low, mid}, region of 2s{high, n - 1}
         //uncertain region or unsorted region{mid, high}.
         //we need to shrink this region{mid, high}.
